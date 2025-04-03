80c34d00cf84b75a2f373b8637a515d7fb67794b
thaisagvilaca <thaisa_vilaca@hotmail.com>
2025-04-03 09:04:34 -0300

    perimetro-area-diametro

---
+++ perimetro-area-diametro.py
@@ -0,0 +1,18 @@
+pi = 3.14
+
+raio = float(input("Digite a medida do raio do círculo (em cm): "))
+
+perimetro = float(2*pi)*raio
+
+area = float(pi*(raio**2))
+
+diametro = float(2*raio)
+
+print(f"O perimetro do circulo é: {perimetro:.2f} cm")
+
+print(f"A area do circulo é: {area:.2f} cm")
+
+print(f"O diametro do circulo é: {diametro:.2f} cm")
+
+
+
